af15bdcd0023428e6e1ad652a3348a268213d3dc
Bhushan Patil <bhupatil0001@gmail.com>
2021-05-11 16:55:22 +0530

    Refactored: in controller layer delete and get by book id

--- OnlineBookStore/src/main/java/com/bookstore/onlinebookstore/controller/BookController.java
+++ OnlineBookStore/src/main/java/com/bookstore/onlinebookstore/controller/BookController.java
@@ -19,23 +19,29 @@ import io.swagger.annotations.ApiOperation;
 @RequestMapping("/book")
 public class BookController {
 
-    @Autowired
-    public IBookStoreService bookStoreService;
+	@Autowired
+	public IBookStoreService bookStoreService;
 
-    @ApiOperation("For getting all books")
-    @GetMapping("/getBooks")
-    public ResponseEntity<Response> getAllBooks() {
-        List<Book> booksList = bookStoreService.getAllBooks();
-        if (booksList != null)
-            return new ResponseEntity<>(new Response( "Returned all books successfully", booksList), HttpStatus.OK);
-        return new ResponseEntity<>(new Response( "Don't have any books!!"), HttpStatus.NOT_ACCEPTABLE);
-    }
+	@ApiOperation("For getting all books")
+	@GetMapping("/getBooks")
+	public ResponseEntity<Response> getAllBooks() {
+		List<Book> booksList = bookStoreService.getAllBooks();
+		if (booksList != null)
+			return new ResponseEntity<>(new Response("Returned all books successfully", booksList), HttpStatus.OK);
+		return new ResponseEntity<>(new Response("Don't have any books!!"), HttpStatus.NOT_ACCEPTABLE);
+	}
 
+	@ApiOperation("For Inserting a book")
+	@PostMapping("/create")
+	public ResponseEntity<Response> createBookData(@RequestBody BookDTO bookDTO) {
+		Book booksList = bookStoreService.createBookData(bookDTO);
+		return new ResponseEntity<>(new Response("Inserted book data successfully!!", booksList), HttpStatus.OK);
+	}
 
-    @ApiOperation("For Inserting a book")
-    @PostMapping("/create")
-    public ResponseEntity<Response> createBookData(@RequestBody BookDTO bookDTO) {
-        Book booksList = bookStoreService.createBookData(bookDTO);
-        return new ResponseEntity<>(new Response( "Inserted book data successfully!!", booksList), HttpStatus.OK);
-    }
+	@ApiOperation("For deleting a book details by book id")
+	@DeleteMapping("/delete/{bookId}")
+	public ResponseEntity<Response> deleteBookDataByBookId(@PathVariable("bookId") long bookId) {
+		bookStoreService.deleteBookDataByBookId(bookId);
+		return new ResponseEntity<>(new Response("Deleted successfully!!", "Deleted id:" + bookId), HttpStatus.OK);
+	}
 }
