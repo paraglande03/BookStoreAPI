666b6790554d9309e37444a6bec24e6af03072cd
Bhushan Patil <bhupatil0001@gmail.com>
2021-05-11 23:01:17 +0530

    Refactored: in naming convention

--- OnlineBookStore/src/main/java/com/bookstore/onlinebookstore/dto/RegistrationDto.java
+++ OnlineBookStore/src/main/java/com/bookstore/onlinebookstore/dto/RegistrationDto.java
@@ -3,30 +3,29 @@ package com.bookstore.onlinebookstore.dto;
 import javax.validation.constraints.Pattern;
 
 import lombok.AllArgsConstructor;
-import org.intellij.lang.annotations.Pattern;
 
 @AllArgsConstructor
 public class RegistrationDto {
 
-    @Pattern(regexp = "^[A-Z][a-z]+\\s?[A-Z][a-z]+$", message = "Please Enter Valid FullName")
-    private String fullName;
+	@Pattern(regexp = "^[A-Z][a-z]+\\s?[A-Z][a-z]+$", message = "Please Enter Valid FullName")
+	private String fullName;
 
-    @Pattern(regexp = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$", message = "EmailId Should follow this pattern abc.xyz@gmail.com.in")
-    private String emailId;
+	@Pattern(regexp = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$", message = "EmailId Should follow this pattern abc.xyz@gmail.com.in")
+	private String emailId;
 
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,20}$", message = "Password length should be 8 must contain at least one uppercase, lowercase, special character and number")
-    private String password;
+	@Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,20}$", message = "Password length should be 8 must contain at least one uppercase, lowercase, special character and number")
+	private String password;
 
-    public String getFullName() {
-        return fullName;
-    }
+	public String getFullName() {
+		return fullName;
+	}
 
-    public String getEmailId() {
-        return emailId;
-    }
+	public String getEmailId() {
+		return emailId;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
 }
--- OnlineBookStore/src/main/java/com/bookstore/onlinebookstore/model/Cart.java
+++ OnlineBookStore/src/main/java/com/bookstore/onlinebookstore/model/Cart.java
@@ -10,30 +10,35 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "cart_items")
-public @Data class Cart {
+public class Cart {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long cartId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long cartId;
 
-    @ManyToOne
-    @JoinColumn(name = "book_id")
-    private Book book;
+	@ManyToOne
+	@JoinColumn(name = "book_id")
+	private Book book;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
 
-    private Integer orderQuantity;
+	private Integer orderQuantity;
 
-    private boolean isInWishList;
+	private boolean isInWishList;
 
-    @Transient
-    public double getSubTotal() {
-        return this.book.getPrice() * orderQuantity;
-    }
+	@Transient
+	public double getSubTotal() {
+		return this.book.getPrice() * orderQuantity;
+	}
 }
