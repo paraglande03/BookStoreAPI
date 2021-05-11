ad08fc7dffb94c63688b198ba2b6d956e3fc83db
IamSanketMore <sanketmore.999.m@gmail.com>
2021-05-11 17:25:55 +0530

    [ADDED] : count method and getBookByName method in book controller

--- OnlineBookStore/src/main/java/com/bookstore/onlinebookstore/repository/BookStoreRepository.java
+++ OnlineBookStore/src/main/java/com/bookstore/onlinebookstore/repository/BookStoreRepository.java
@@ -3,9 +3,14 @@ package com.bookstore.onlinebookstore.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bookstore.onlinebookstore.model.Book;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookStoreRepository extends JpaRepository<Book, Long> {
 
+    @Query(value = "select * from book where book_name LIKE %?1% ", nativeQuery = true)
+    public List<Book> findBooksByBookName(String bookName);
 }
