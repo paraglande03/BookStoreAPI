cceffebfe000790ed2b4b77259e3080f14cbf25e
paraglande03 <paraglande03@gmail.com>
2021-05-11 20:41:53 +0530

    Added : cart service for updating order quantity

--- OnlineBookStore/src/main/java/com/bookstore/onlinebookstore/service/ICartService.java
+++ OnlineBookStore/src/main/java/com/bookstore/onlinebookstore/service/ICartService.java
@@ -5,4 +5,6 @@ import com.bookstore.onlinebookstore.model.Cart;
 public interface ICartService {
 
     public Cart addBookToCart(String token, Long bookId, Integer order_quantity);
+
+    public String updateOrderQuantity(Long bookId, Integer order_quantity, String token);
 }
