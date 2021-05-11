6a8f93b7328dc3fa5e990fd6e91dfd694b447985
IamSanketMore <sanketmore.999.m@gmail.com>
2021-05-11 15:45:57 +0530

    [ADDED] : Added DTO layer for BookDTO

---
+++ OnlineBookStore/src/main/java/com/bookstore/onlinebookstore/dto/BookDTO.java
@@ -0,0 +1,18 @@
+package com.bookstore.onlinebookstore.dto;
+
+import lombok.ToString;
+
+@ToString
+public class BookDTO
+{
+    public String authorName;
+
+    public String bookName;
+
+    public int quantity;
+
+    public double price;
+
+    public String bookDetails;
+
+}
